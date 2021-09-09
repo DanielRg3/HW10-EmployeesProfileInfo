b5fe1738fd510f81952e554d23d0b47c1f0699f0
Daniel Ruiz <danielruizgtz@gmail.com>
2021-09-08 23:09:51 -0500

    HW#10 Employee Profile
    In this commit we add the tests

--- _test/Manager.test.js
+++ _test/Manager.test.js
@@ -1,13 +1,13 @@
 const Manager = require("../lib/Manager");
 
-est("Recieve Manager's Name", () => {
-    const name = "Daniel";
-    const e = new Manager(name);
-    expect(e.name).toBe(name);
+test("Recieve Manager's Name", () => {
+    const expectedValue = "Daniel";
+    const e = new Manager("Daniel", 2, "daniel@gmail.com", 1234567892);
+    expect(e.getName()).toBe(expectedValue);
 });
 
-test("Recieve an ID different than void", () => {
-    const id = "123";
-    const e = new Manager(id);
-    expect(e.id).toBe(id);
+test("Recieve Manager's ID", () => {
+    const expectedValue = 2;
+    const e = new Manager("Daniel", 2, "daniel@gmail.com", 1234567892);
+    expect(e.getId()).toBe(expectedValue);
 });
